2208f34379d9412cf9ccf58030fd600f75f52208
Maris <mcclapperton@gmail.com>
2022-12-13 07:29:12 -0700

    exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
-
+    validates_presence_of :first_name
+    validates_presence_of :last_name
+    validates_inclusion_of :hourly_rate, in: 40..200
+    validates_presence_of :store_id
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
-
+  validates_length_of :name, minimum: 3
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
 end
